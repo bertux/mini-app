bf125ad344b38c7e49c6ac14e368804f218c626d
Bertrand Juglas <b@juglas.name>
2024-12-09 22:51:08 +0100

    set manifest URL and add logo

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -46,7 +46,7 @@ function RootInner({ children }: PropsWithChildren) {
   }, [initDataUser]);
 
   return (
-    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://mini-app-next.vercel.app/tonconnect-manifest.json">
       <AppRoot
         appearance={isDark ? 'dark' : 'light'}
         platform={['macos', 'ios'].includes(lp.platform) ? 'ios' : 'base'}
